36e592623a9d00d89e49488efdb8243b2915f65d
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2025-07-25 17:59:12 +0200

    LUT-30459 : Adding liquibase tags to SQL scripts (Lutèce 8)

--- src/sql/plugins/modulenotifygrumappingmanager/upgrade/update_db_modulenotifygrumappingmanager-1.0.2-1.0.3.sql
+++ src/sql/plugins/modulenotifygrumappingmanager/upgrade/update_db_modulenotifygrumappingmanager-1.0.2-1.0.3.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+--changeset modulenotifygrumappingmanager:update_db_modulenotifygrumappingmanager-1.0.2-1.0.3.sql
+--preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE workflow_task_notify_gru_mapping_manager ADD COLUMN connection_id int(11) NOT NULL default '0' AFTER beankey;
 ALTER TABLE workflow_task_notify_gru_mapping_manager ADD COLUMN customer_id int(11) NOT NULL default '0' AFTER connection_id;
 ALTER TABLE workflow_task_notify_gru_mapping_manager ADD COLUMN demand_reference int(11) NOT NULL default '0' AFTER demandetype;
