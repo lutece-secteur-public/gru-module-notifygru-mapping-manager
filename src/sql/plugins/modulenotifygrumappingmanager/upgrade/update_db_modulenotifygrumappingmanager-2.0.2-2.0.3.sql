135341d7531dc2878ac6147d32d382547bcb0188
Jeremy Deloche <jeremy.deloche@atos.com>
2025-11-20 10:02:11 +0100

    LUT-31243 - Liquibase tags correction

--- src/sql/plugins/modulenotifygrumappingmanager/upgrade/update_db_modulenotifygrumappingmanager-2.0.2-2.0.3.sql
+++ src/sql/plugins/modulenotifygrumappingmanager/upgrade/update_db_modulenotifygrumappingmanager-2.0.2-2.0.3.sql
@@ -1,4 +1,4 @@
---liquibase formatted sql
---changeset modulenotifygrumappingmanager:update_db_modulenotifygrumappingmanager-2.0.2-2.0.3.sql
---preconditions onFail:MARK_RAN onError:WARN
+-- liquibase formatted sql
+-- changeset modulenotifygrumappingmanager:update_db_modulenotifygrumappingmanager-2.0.2-2.0.3.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE workflow_task_notify_gru_mapping_manager MODIFY COLUMN id_notifygrumappingmanager int AUTO_INCREMENT;
